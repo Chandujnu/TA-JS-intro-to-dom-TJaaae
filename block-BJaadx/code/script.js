355bacaae2dfe7b8d99a5b8b320148b18df117c5
Chandan Kumar <chandanchandu287@gmail.com>
2021-11-14 23:32:22 +0530

    Refactor code

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -11,10 +11,11 @@ let firstUlLi = document.body.children[5].children[0];
 console.dir(firstUlLi);
 firstUlLi.innerText = "all about document";
 //    - Select the input element with name `email`
-document.body.children[6].children[1];
+document.body.children[6].children[2];
 
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-console.log(document.body.children[5].children);
+let ulElement = document.body.children[5];
+console.log(`The number of children of ul is ${ulElement.children.length}`)
 
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
 let emailInput = document.querySelector("input");
@@ -25,21 +26,23 @@ let label = document.querySelector("label");
 // 7. Select the input of type `checkbox` with the `id` selector and store in `inputCheckbox`
 let inputCheckbox = document.querySelector("#remember");
 // 8. Select the input of type password using Attribute selectors. (eg: input[type="text"]) and store in `password`
-let password = document.querySelector("input");
+let password = document.querySelector(`input[type = "password"]`);
+
 // 9. Select the input using the placeholder attribute selector with value `password` and store in `attrPassword`
-let attrPassword = document.querySelector("#remember");
+let attrPassword = document.querySelector(`input[placeholder = "password"]`);
 // 10. Select all the `li` element and store in `allTopics`
 let allTopics = document.querySelectorAll("li");
 // 11. Select all the input element of any type and store in `allInput`
 let allInput = document.querySelectorAll("input");
 // 12. Use forEach to console the `innerText` property of all the li element in `allTopics` variable.
 console.dir(allTopics);
-allTopics.forEach((element) => allTopics);
+allTopics.forEach((element) => console.log(element.innerText));
 // 13. Select all the elements with class `list` and store in variable `listOfSelectedTopics`
 let listOfSelectedTopics = document.querySelectorAll(".list");
 // 14. Select the first li element inside the `ul` element using `>` (direct child) and store in `firstLi`
-let ul = document.querySelector("ul");
-let firstLi = ul.firstElementChild;
+// let ul = document.querySelector("ul");
+// let firstLi = ul.firstElementChild;
+let firstLi = document.querySelector(`ul>li`);
 // 15. Select all the img element and log the number of element saying `The total number of img element is ---`
 let allImgElement = document.querySelectorAll("img");
 console.dir(allImgElement);
@@ -59,16 +62,16 @@ let allElementWithIdTest = document.querySelectorAll("#test");
 document.getElementById("test");
 
 // 21. Select the parent element of the element stored in `topics` variable (#5) and log the element.
-let topicsElement = document.querySelectorAll("li")
-console.log(topicsElement.parentElement);
+console.log(topics.parentElement);
 // 22. Select the next element sibling of the element stored in `topics` variable (#5) and log the element.
-
+console.log(topics.nextElementSibling);
 // 23. Select the previous element sibling of the element stored in `topics` variable (#5) and change the `innerText` property to `Learning About Walking the DOM`.
-
+console.log(topics.previousElementSibling.innerText = `Learning About Walking the DOM`);
 // 24. Select the first element child of the element stored in `topics` variable (#5) and change the `innerText` property of the element to `This is the first child element`.
-
+console.log(topics.firstElementChild.innerText = `This is the first child element`);
 // 25. Select the last element child of the element stored in `topics` variable (#5) and log the `typeof` the element.
-
+console.log(typeof topics.lastElementChild);
 // 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
-
+let fieldsetElm = document.querySelector("fieldset");
 // 27. Select the parent element of the element stored in `fieldsetElm` variable (#5) and log the `typeof` the element.
+console.log(typeof fieldsetElm.parentElement);
