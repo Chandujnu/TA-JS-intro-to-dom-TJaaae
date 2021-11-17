f9d64d9c2001dca9aea24c7aedc76c3cc149c9bc
Chandan Kumar <chandanchandu287@gmail.com>
2021-11-17 23:14:10 +0530

    Refactor code

--- block-BJaaef/code/script.js
+++ block-BJaaef/code/script.js
@@ -1,1 +1,17 @@
-got.houses.forEach
+
+let parentElm = document.querySelector("ul");
+
+let allPeople = got.houses.reduce((acc, cv) => {
+    acc = acc.concat(cv.people);
+    return acc;
+},[]);
+
+
+parentElm.innerHTML = allPeople.map((person) => {
+    return `<li>
+    <img src="${person.image}" alt="${person.name}">
+    <span>${person.name}</span>
+    <p>${person.description}</p>
+    <button>Learn More!</button>
+  </li>`;
+}).join("");
