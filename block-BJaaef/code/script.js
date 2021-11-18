1c1ea298d1977990112456fb91b633ea0481d1e6
Chandan Kumar <chandanchandu287@gmail.com>
2021-11-18 11:48:41 +0530

    Refactor code

--- block-BJaaef/code/script.js
+++ block-BJaaef/code/script.js
@@ -1,5 +1,5 @@
-
-let parentElm = document.querySelector("ul");
+console.log(got);
+let parentElm = document.querySelector(".box");
 
 let allPeople = got.houses.reduce((acc, cv) => {
     acc = acc.concat(cv.people);
@@ -7,11 +7,30 @@ let allPeople = got.houses.reduce((acc, cv) => {
 },[]);
 
 
-parentElm.innerHTML = allPeople.map((person) => {
+let cardsHTML = allPeople.map((person) => {
     return `<li>
     <img src="${person.image}" alt="${person.name}">
     <span>${person.name}</span>
     <p>${person.description}</p>
     <button>Learn More!</button>
   </li>`;
-}).join("");
+});
+
+parentElm.innerHTML = cardsHTML.join("");
+
+// let ul = document.querySelector("ul")
+
+// allPeople.map((oerson) => {
+//   let li = document.createElement("li");
+//   let img = document.createElement("img");
+//   img.src = "person.image";
+//   let span = document.createElement("span");
+//   span.innerText = "person.name";
+//   let p = document.createElement("p");
+//   p.innerText = "person.description";
+//   let button = document.createElement("button");
+//   button.innerText = "Learn More!";
+
+//   li.append(img, span, p, button);
+//   ul.append(li)
+// })
